8bc336695a7b814379559441600d647ab0d04137
Code Monsters <info@codemonsters.es>
2014-10-13 19:10:36 +0200

    Added test audio files. Updated audiomd.py

--- audiomd.py
+++ audiomd.py
@@ -1,39 +1,68 @@
 #!/usr/bin/env python3
+import glob
+import os
+import sys
+
 __author__ = 'micomicona'
 
-#import getopt,os,sys
-import os,sys
 
 def main(argv):
-    """
-    :param argv:
-    :return:
-    """
+
     errors = []
     commands = []
     arguments = []
+    # Flag used to indicate that the program is reading file names
+    # Once this happens no more commands should be added
+    # In other words: force program usage to first accept commands and later file names
+    reading_arguments = False;
+    # Flag used to indicate if the program should traverse directories recursively
+    recursive = False;
+
     if len(argv) == 0:
-        printusage()
+        print_usage()
         sys.exit(2)
+
     for arg in argv:
         if arg.lower() in ('-h', '--help'):
-            printusage()
+            if reading_arguments:
+                errors.append("Commands should be specified before file names (%s)" % arg)
+            print_usage()
             sys.exit(2)
         elif arg.lower() in ("-g", "--replaygain"):
-            commands.append(replayGain)
+            if reading_arguments:
+                errors.append("Commands should be specified before file names (%s)" % arg)
+            commands.append(replay_gain)
+        elif arg.lower() in ("-r", "--recursive"):
+            recursive = True;
         else:
+            reading_arguments = True
             arguments.append(arg)
 
     if len(commands) == 0:
         errors.append("Nothing to do! Please specify at least one command")
     if len(arguments) == 0:
         errors.append("No files specified")
+
+    if recursive:
+        for path, dirs, files in os.walk("."):
+            print("PATH:" + path)
+
+            #for argument in arguments:
+            #    for f in glob.glob(argument):
+            #        print(f)
+
+            #for f in files:
+            #    print(f)
+    for argument in arguments:
+        for f in glob.glob(argument):
+            print(f)
+
     if len(errors) > 0:
         print("Errors found. Please check the following messages for more information:")
         for error in errors:
             print(error)
         print()
-        printusage()
+        print_usage()
         sys.exit(2)
     '''
     try:
@@ -52,12 +81,15 @@ def main(argv):
     #print('Files: ', args)
     '''
 
-def printusage():
+
+def print_usage():
     print('%s [COMMANDS]... [FILES]...' % (os.path.basename(__file__)))
     print('\nSupported commands:')
     print('  -g, --replaygain             Set ReplayGain values')
+    print('  -r, --recursive              Recursively traverse directories')
 
-def replayGain():
+
+def replay_gain():
     print("Calcular ReplayGain")
 
 if __name__ == "__main__":
